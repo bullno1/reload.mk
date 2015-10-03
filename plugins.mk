8984e4e500203240174786582c3799fdbf7b0e7f
bullno1 <thebullno1@gmail.com>
2015-10-04 04:41:18 +0800

    Make it possible to use reload.mk without adding it as a runtime dep.

--- plugins.mk
+++ plugins.mk
@@ -11,13 +11,23 @@ watch_verbose = $(watch_verbose_$(V))
 reload_verbose_0 = @echo " RELOAD" $(PROJECT);
 reload_verbose = $(reload_verbose_$(V))
 
-reload: app
-	$(reload_verbose) $(RELX_OUTPUT_DIR)/$(RELX_RELEASE)/bin/$(RELX_RELEASE) rpcterms reload_mk reload
+RELOAD_MK_RPC = $(RELX_OUTPUT_DIR)/$(RELX_RELEASE)/bin/$(RELX_RELEASE) rpcterms
+BUILD_DEPS += reload_mk
 
-auto-reload:
+reload: deps app bootstrap-reload.mk
+	$(reload_verbose) $(RELOAD_MK_RPC) reload_mk reload
+
+auto-reload: bootstrap-reload.mk
 	$(watch_verbose) \
-	while :; \
-	do \
-		inotifywait -q -e close_write $(RELOAD_MK_WATCH_DIRS) > /dev/null; \
-		make reload; \
-	done;
+		while :; \
+		do \
+			inotifywait -q -e close_write $(RELOAD_MK_WATCH_DIRS) > /dev/null; \
+			make reload; \
+		done;
+
+bootstrap-reload.mk:
+	$(verbose) \
+		if [ $$($(RELOAD_MK_RPC) code is_loaded reload_mk.) = false ]; then \
+			$(RELOAD_MK_RPC) code add_pathz '"$(DEPS_DIR)/reload_mk/ebin/".'; \
+			$(RELOAD_MK_RPC) code load_abs '"$(DEPS_DIR)/reload_mk/ebin/reload_mk".'; \
+		fi > /dev/null;
