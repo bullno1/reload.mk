d58fb92ec7d584c82d6db2c8b550e196068e36cb
bullno1 <thebullno1@gmail.com>
2015-10-03 14:48:32 +0800

    Implement auto-reload

--- plugins.mk
+++ plugins.mk
@@ -1,4 +1,23 @@
+# A list of directories that "make auto-reload" should watch
+RELOAD_MK_WATCH_DIRS ?= src
+
+# Internal
+
 .PHONY: reload
 
+watch_verbose_0 = @echo " WATCH " $(RELOAD_MK_WATCH_DIRS);
+watch_verbose = $(watch_verbose_$(V))
+
+reload_verbose_0 = @echo " RELOAD " $(PROJECT);
+reload_verbose = $(reload_verbose_$(V))
+
 reload: app
-	$(verbose) $(RELX_OUTPUT_DIR)/$(RELX_RELEASE)/bin/$(RELX_RELEASE) rpcterms reload_mk reload
+	$(reload_verbose) $(RELX_OUTPUT_DIR)/$(RELX_RELEASE)/bin/$(RELX_RELEASE) rpcterms reload_mk reload
+
+auto-reload:
+		$(watch_verbose) \
+		while :; \
+		do \
+			inotifywait -q -e close_write $(RELOAD_MK_WATCH_DIRS) > /dev/null; \
+			make reload; \
+		done;
